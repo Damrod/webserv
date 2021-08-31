9ba26fafd5dd87561c45d4b19a49e52634ab07d1
gbudau <54190400+gbudau@users.noreply.github.com>
2021-08-31 23:42:27 +0200

    Update Makefile
    Add -g3 flag for debugging to Makefile
    Remove placeholder test file and create a test main that contain only
    the catch2 main
    Update Makefile to compile multiple test files

--- srcs/target/common.mk
+++ srcs/target/common.mk
@@ -1,7 +1,9 @@
+INC_PARAMS = $(foreach d, $(INC_DIR), -I$d)
+
 define generateRules
 $(1)/%.o: %.cpp
 	@echo Building $$@
-	$(CXX) -c $(CXXFLAGS) -I $(INC_DIR) -o $$@ $$< -MMD
+	$(CXX) -c $(CXXFLAGS) $(INC_PARAMS) -o $$@ $$< -MMD
 endef
 
 define generateDirs
@@ -14,3 +16,8 @@ endef
 SRC_DIRS := $(foreach dir, $(DIRS), $(addprefix $(SRC_DIR)/, $(dir)))
 # this will replace the above srcs/ part of the path with bld/
 OBJ_DIRS := $(foreach dir, $(DIRS), $(addprefix $(BLD_DIR)/, $(dir)))
+
+# this will glob all cpp files inside the SRC_DIRS
+SRCS := $(foreach dir,$(SRC_DIRS),$(wildcard $(dir)/*.cpp))
+# this is here so that the macro generateRules can find its sources
+VPATH := $(SRC_DIRS)
--- srcs/target/test.mk
+++ srcs/target/test.mk
@@ -4,9 +4,8 @@ $(TEST0) : $(OBJ_DIRS) $(OBJ)
 	make -C srcs/app/test
 
 .PHONY: run
-run:
+run: test
 	$(BLD_DIR)/utest_app
 
 .PHONY: test
-test:
-	make $(TEST0)
+test: $(TEST0)
