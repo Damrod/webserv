7a082655a6c5640c044d8addeed8b3d16e1e9552
gbudau <54190400+gbudau@users.noreply.github.com>
2021-09-11 21:23:58 +0200

    [HttpResponse] Add HttpResponse class
    Create a HTTP response with status line, headers and body

---
+++ srcs/app/test/http/HttpResponseTest.cpp
@@ -0,0 +1,68 @@
+#include <sstream>
+#include <string>
+#include <HttpResponse.hpp>
+#include <catch2.hpp>
+
+TEST_CASE("InvalidHttpResponseInvalidStatus", "[http]") {
+	REQUIRE_THROWS(HttpResponse(42));
+}
+
+TEST_CASE("ValidHttpResponseWithoutHeadersAndBody", "[http]") {
+	const std::string str = "HTTP/1.1 200 OK\r\n"
+							"\r\n";
+	HttpResponse	response(200);
+
+	REQUIRE("HTTP/1.1" == response.GetHttpVersion());
+	REQUIRE(200 == response.GetStatusCode());
+	REQUIRE("OK" == response.GetReasonPhrase());
+}
+
+TEST_CASE("ValidHttpResponseWithoutBody", "[http]") {
+	const std::string str = "HTTP/1.1 200 OK\r\n"
+							"server: webserv\r\n"
+							"\r\n";
+	HttpResponse	response(200);
+
+	response.AddHeader("Server", "webserv");
+	REQUIRE("HTTP/1.1" == response.GetHttpVersion());
+	REQUIRE(200 == response.GetStatusCode());
+	REQUIRE("OK" == response.GetReasonPhrase());
+	REQUIRE("webserv" == response.GetHeaderValue("Server"));
+	REQUIRE(str == response.CreateResponseString());
+}
+
+TEST_CASE("ValidHttpResponseWithBody", "[http]") {
+	const std::string	body = "Hello world!";
+	std::stringstream	ss;
+	ss << body.size();
+	const std::string	body_size_str = ss.str();
+
+	std::string	str = "HTTP/1.1 200 OK\r\n";
+
+	typedef std::map<std::string, std::string>	HeadersMap;
+	HeadersMap	headers;
+
+	headers.insert(std::make_pair("server", "webserv"));
+	headers.insert(std::make_pair("content-length", body_size_str));
+	headers.insert(std::make_pair("content-type", "text/html"));
+	HeadersMap::const_iterator	it = headers.begin();
+	HeadersMap::const_iterator	ite = headers.end();
+	while (it != ite) {
+		str += it->first + ": " + it->second + "\r\n";
+		++it;
+	}
+	str += "\r\n" + body;
+
+	HttpResponse	response(200);
+
+	response.AddHeader("Server", "webserv");
+	response.AddHeader("Content-Length", body_size_str);
+	response.AddHeader("Content-Type", "text/html");
+	response.SetBody(body);
+	REQUIRE("HTTP/1.1" == response.GetHttpVersion());
+	REQUIRE(200 == response.GetStatusCode());
+	REQUIRE("OK" == response.GetReasonPhrase());
+	REQUIRE("webserv" == response.GetHeaderValue("Server"));
+	REQUIRE(str == response.CreateResponseString());
+	REQUIRE(headers == response.GetHeaders());
+}
