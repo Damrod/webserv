a6eeb91d79af547139948b6c85c2784d3ec649c5
gbudau <54190400+gbudau@users.noreply.github.com>
2021-09-16 23:19:08 +0200

    [CommonConfig] Update the variables
    Added some of the variables required for the server configuration

--- srcs/app/config/CommonConfig.cpp
+++ srcs/app/config/CommonConfig.cpp
@@ -2,5 +2,6 @@
 
 CommonConfig::CommonConfig()
 	: root("html"), client_max_body_size(1000000), autoindex(false),
-	index("index.html") {
+	index("index.html"), error_pages(), upload_store(""), return_status(),
+	return_url(""), cgi_assign() {
 }
--- srcs/incs/CommonConfig.hpp
+++ srcs/incs/CommonConfig.hpp
@@ -1,13 +1,29 @@
 #ifndef SRCS_INCS_COMMONCONFIG_HPP_
 #define SRCS_INCS_COMMONCONFIG_HPP_
 #include <stdint.h>
+#include <map>
 #include <string>
+#include <vector>
 
 struct CommonConfig {
-	std::string	root;
-	uint32_t	client_max_body_size;
-	bool		autoindex;
-	std::string	index;
+	// typedefs
+	typedef std::string									FileExtension;
+	typedef	std::string									BinaryHandlerPath;
+	typedef std::map<FileExtension, BinaryHandlerPath>	CgiAssignMap;
+	typedef uint16_t									ErrorPageCode;
+	typedef std::string									ErrorPageUri;
+	typedef std::map<ErrorPageCode, ErrorPageUri>		ErrorPagesMap;
+
+	// directives
+	std::string		root;
+	uint32_t		client_max_body_size;
+	bool			autoindex;
+	std::string		index;
+	ErrorPagesMap	error_pages;
+	std::string		upload_store;
+	uint16_t		return_status;
+	std::string		return_url;
+	CgiAssignMap	cgi_assign;
 
 	CommonConfig();
 };
