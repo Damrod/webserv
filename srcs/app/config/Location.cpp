52d1aef99c71703569dbaf1baf2ce4f36a7cf53c
gbudau <54190400+gbudau@users.noreply.github.com>
2021-09-17 19:11:29 +0200

    [Location] Add limit_except variable
    Move Location to config directory

---
+++ srcs/app/config/Location.cpp
@@ -0,0 +1,5 @@
+#include <Location.hpp>
+
+Location::Location(const CommonConfig &common)
+	: common(common), limit_except() {
+}
--- srcs/incs/Location.hpp
+++ srcs/incs/Location.hpp
@@ -1,11 +1,13 @@
 #ifndef SRCS_INCS_LOCATION_HPP_
 #define SRCS_INCS_LOCATION_HPP_
 #include <string>
+#include <vector>
 #include <CommonConfig.hpp>
 
 struct	Location {
-	CommonConfig	common;
-	/* More location settings */
+	CommonConfig			common;
+	typedef std::string		HttpMethod;
+	std::vector<HttpMethod>	limit_except;
 
 	explicit Location(const CommonConfig &common);
 };
