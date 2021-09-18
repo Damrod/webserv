f38f7c289360dc23b045c004367aaad2944b1553
gbudau <54190400+gbudau@users.noreply.github.com>
2021-09-18 23:39:56 +0200

    [CommonConfig] Update constructor
    Removed the variables that are either default or zero initialized
    from the list initialization of the constructor

--- srcs/app/config/CommonConfig.cpp
+++ srcs/app/config/CommonConfig.cpp
@@ -2,6 +2,5 @@
 
 CommonConfig::CommonConfig()
 	: root("html"), client_max_body_size(1000000), autoindex(false),
-	index("index.html"), error_pages(), upload_store(""), return_status(),
-	return_url(""), cgi_assign() {
+	index("index.html") {
 }
