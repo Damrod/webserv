6ab19d5579c49e1f36e31fade840e69366a32df2
Alvaro Ollero <aollero@gmail.com>
2021-09-22 00:24:19 +0300

    added missing header

---
+++ srcs/incs/parser/Preprocessor.hpp
@@ -0,0 +1,30 @@
+#ifndef SRCS_INCS_PARSER_PREPROCESSOR_HPP_
+#define SRCS_INCS_PARSER_PREPROCESSOR_HPP_
+
+# include <cerrno>
+# include <iostream>
+# include <fstream>
+# include <cstring>
+# include <string>
+# include <algorithm>
+# include <cctype>
+# include <list>
+# include <map>
+# include <vector>
+# include <sstream>
+# include <parser/Analyser.hpp>
+
+class Preprocessor : public Analyser {
+ public:
+	explicit Preprocessor(const std::string &path);
+	virtual ~Preprocessor(void);
+	std::string preprocess(std::ifstream &file);
+	std::string &GetFileBuffer(void);
+ private:
+	std::string				path_;
+	std::string				filebuff_;
+};
+
+std::ostream &operator<<(std::ostream &o, Preprocessor const &i);
+
+#endif  // SRCS_INCS_PARSER_PREPROCESSOR_HPP_
