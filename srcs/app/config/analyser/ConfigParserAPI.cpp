06f694dd25dfdd05bc251a687cecc3c960902e02
Alvaro Ollero <aollero@gmail.com>
2021-09-22 01:29:05 +0300

    fixed conflict with latest lexer

--- srcs/app/config/analyser/ConfigParserAPI.cpp
+++ srcs/app/config/analyser/ConfigParserAPI.cpp
@@ -5,7 +5,7 @@ ConfigParserAPI::ConfigParserAPI(const std::string &path) : path_(path) {}
 std::vector<ServerConfig> ConfigParserAPI::GetServersSettings(void) {
 	Preprocessor file(path_);
 	Lexer lexed(file.GetFileBuffer());
-	std::list<Token> tokens = *lexed.GetTokens();
+	std::list<Token> tokens = lexed.GetTokens();
 	std::vector<ServerConfig> servers_settings;
 	ParserAPI config(&servers_settings);
 	Parser parser(tokens, &config);
