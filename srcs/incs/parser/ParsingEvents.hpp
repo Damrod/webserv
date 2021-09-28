8f3d3197d73b2588c47175d68ac5fdebefc7edbd
Alvaro Ollero <aollero@gmail.com>
2021-09-28 22:16:04 +0300

    ready for review possibly

--- srcs/incs/parser/ParsingEvents.hpp
+++ srcs/incs/parser/ParsingEvents.hpp
@@ -10,21 +10,11 @@ class ParsingEvents {
  public:
 	enum e_id {
 		EV_NONE = -1,
-		IP = 0,
-		PORT = 1,
-		IP_PORT = 2,
-		DIR = 3,
-		SIZE = 4,
-		URI = 5,
-		URL = 6,
-		ERROR_CODE = 7,
-		ON_OFF = 8,
-		FILE = 9,
-		METHOD = 10,
-		OPEN = 11,
-		CLOSE = 12,
-		SEMIC = 13,
-		KEYWORD = 14,
+		KEYWORD,
+		WORD,
+		OPEN,
+		CLOSE,
+		SEMIC,
 		INVALID
 	};
 	static enum e_id GetEvent(const Token &token);
