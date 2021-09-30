4a4fef2abd331f5fd2a104da5ca0cfeaba066893
gbudau <54190400+gbudau@users.noreply.github.com>
2021-10-01 00:36:17 +0200

    [MockConfigLoadFile] Add another server on 8084
    Testing upload, CGI and HTTP DELETE method

--- srcs/app/config/MockConfigLoadFile.cpp
+++ srcs/app/config/MockConfigLoadFile.cpp
@@ -12,30 +12,29 @@ std::vector<ServerConfig>	MockConfigLoadFile::GetServersConfigs() const {
 
 /*
 server {
-    listen			8080;
-    server_name		localhost;
+	listen			8080;
+	server_name		localhost;
 	autoindex		on;
 	index			nonexistent.html;
 }
 
 server {
-    listen			8081;
-    server_name		localhost;
-    root			html/web1;
+	listen			8081;
+	server_name		localhost;
+	root			html/web1;
 
-	location		/web2 {
-		return 301	http://localhost:8082;
+	location		/google {
+		return 301	https://www.google.com;
 	}
-
 }
 
 server {
-    listen			8082;
-    server_name		localhost;
-    root			html/web2;
-    error_page 404	/404.html;
+	listen			8082;
+	server_name		localhost;
+	root			html/web2;
+	error_page 404	/404.html;
 
-	location		/web1 {
+	location		/index/ {
 		root		html;
 		index		nonexistent.html;
 		autoindex	on;
@@ -43,44 +42,101 @@ server {
 }
 
 server {
-    listen			8083;
+	listen			8083;
+	return 301		https://www.google.com;
+}
+
+server {
+	listen			8084;
+	root			html/web3;
+
+	location	/upload/ {
+		limit_except POST GET;
+		upload_store	html/web3/test/;
+	}
+
+	location	/test/ {
+		limit_except DELETE GET;
+	}
+
+	location	/images/ {
+		limit_except GET POST;
+		client_max_body_size	42;
+	}
+
+	location	/cgi-bin/ {
+		limit_except POST GET;
+		cgi-assign .py	/usr/bin/python;
+	}
 }
 */
 void MockConfigLoadFile::InitConfig_() {
 	// Add the server listening on 8080 with autoindex enable
-	ServerConfig	server1;
+	ServerConfig server1;
 	server1.listen_port = 8080;
 	server1.server_name.push_back("localhost");
 	server1.common.autoindex = true;
 	server1.common.index = "nonexistent.html";
 	servers_configs_.push_back(server1);
 
-	// Add the server listening on 8081, serving the web1 page
-	ServerConfig	server2;
+	// Add the server listening on 8081
+	ServerConfig server2;
 	server2.listen_port = 8081;
 	server2.server_name.push_back("localhost");
 	server2.common.root = "html/web1";
-	Location	web1_redirect("/web2", server2.common);
+
+	Location web1_redirect("/google", server2.common);
 	web1_redirect.common.return_status = 301;
-	web1_redirect.common.return_url = "http://localhost:8082";
+	web1_redirect.common.return_url = "https://www.google.com";
 	server2.locations.push_back(web1_redirect);
 	servers_configs_.push_back(server2);
 
-	// Add the server listening on 8082, serving the web2 page
-	ServerConfig	server3;
+	// Add the server listening on 8082
+	ServerConfig server3;
 	server3.listen_port = 8082;
 	server3.server_name.push_back("localhost");
 	server3.common.root = "html/web2";
 	server3.common.error_pages.insert(std::make_pair(404, "/404.html"));
-	Location	web2_autoindex("/web1", server3.common);
+
+	Location web2_autoindex("/index/", server3.common);
 	web2_autoindex.common.root = "html";
 	web2_autoindex.common.index = "nonexistent.html";
 	web2_autoindex.common.autoindex = true;
 	server3.locations.push_back(web2_autoindex);
 	servers_configs_.push_back(server3);
 
-	// Add the server listening on 8083, accessing the root page results in 403
+	// Add the server listening on 8083, performing an HTTP redirect
 	ServerConfig	server4;
 	server4.listen_port = 8083;
 	servers_configs_.push_back(server4);
+
+	// Add the server listening on 8085
+	ServerConfig server5;
+	server4.listen_port = 8084;
+	server3.common.root = "html/web3";
+
+	Location web3_upload("/upload/", server5.common);
+	web3_upload.limit_except.push_back("POST");
+	web3_upload.limit_except.push_back("GET");
+	web3_upload.common.upload_store = "html/web3/test/";
+	server5.locations.push_back(web3_upload);
+
+	Location web3_test("/test/", server5.common);
+	web3_test.limit_except.push_back("DELETE");
+	web3_test.limit_except.push_back("GET");
+	server5.locations.push_back(web3_test);
+
+	Location web3_images("/images/", server5.common);
+	web3_images.limit_except.push_back("GET");
+	web3_images.limit_except.push_back("POST");
+	web3_images.common.client_max_body_size = 42;
+	server5.locations.push_back(web3_images);
+
+	Location web3_cgi_bin("/cgi-bin/", server5.common);
+	web3_cgi_bin.limit_except.push_back("POST");
+	web3_cgi_bin.limit_except.push_back("GET");
+	web3_cgi_bin.common.cgi_assign.insert(
+									std::make_pair("py", "/usr/bin/python"));
+	server5.locations.push_back(web3_cgi_bin);
+	servers_configs_.push_back(server5);
 }
