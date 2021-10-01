7941e87593ccc9016aa96cf6dad18eb326accb29
gbudau <54190400+gbudau@users.noreply.github.com>
2021-10-01 13:15:09 +0200

    [MimeTypes] Fix tests

--- srcs/app/test/http/MimeTypesTest.cpp
+++ srcs/app/test/http/MimeTypesTest.cpp
@@ -3,10 +3,8 @@
 #include <catch2.hpp>
 
 TEST_CASE("MimeTypes", "[http]") {
-	const MimeTypes	mime_types;
-
-	REQUIRE("text/html" == mime_types.GetMimeType("html"));
-	REQUIRE("text/plain" == mime_types.GetMimeType("txt"));
-	REQUIRE("image/jpeg" == mime_types.GetMimeType("jpg"));
-	REQUIRE("application/octet-stream" == mime_types.GetMimeType("unknown"));
+	REQUIRE("text/html" == MimeTypes::GetMimeType("html"));
+	REQUIRE("text/plain" == MimeTypes::GetMimeType("txt"));
+	REQUIRE("image/jpeg" == MimeTypes::GetMimeType("jpg"));
+	REQUIRE("application/octet-stream" == MimeTypes::GetMimeType("unknown"));
 }
