59fc098cc4290c4c5f087da46e76c44ab7d2b418
gbudau <54190400+gbudau@users.noreply.github.com>
2021-10-08 22:27:23 +0200

    [Connection] Refactor ReadRequest and SendResponse
    Return ReadRequestStatus and SendResponseStatus

--- srcs/incs/Connection.hpp
+++ srcs/incs/Connection.hpp
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string>
+#include <ConnectionIOStatus.hpp>
 #include <HttpRequest.hpp>
 #include <HttpResponse.hpp>
 #include <ServerConfig.hpp>
@@ -10,13 +11,12 @@
 class Connection {
 	public:
 		Connection(const ServerConfig &server_config, int sd);
-		bool	ReadRequest();
-		bool	SendResponse();
+		enum ReadRequestStatus::Type	ReadRequest();
+		enum SendResponseStatus::Type	SendResponse();
 
 	private:
 		const ServerConfig	&server_config_;
 		const int			socket_;
-		bool				ready_for_response_;
 		bool				keep_alive_;
 		std::string			raw_request_;
 		std::string			raw_response_;
