4d701ca9e34d185f729f8b798b281d3c74ccb57e
Alvaro Ollero <aollero@gmail.com>
2021-10-13 12:26:24 +0300

    connected parser to webserv

--- srcs/app/config/Config.cpp
+++ srcs/app/config/Config.cpp
@@ -1,11 +1,11 @@
 #include <Config.hpp>
-#include <MockConfigLoadFile.hpp>
+#include <parser/ParserManager.hpp>
 
 bool	Config::LoadFile(const std::string &pathname) {
 	// Read and parse the configuration file and save to servers_settings_
 	if (!pathname.empty()) {
-		MockConfigLoadFile	mock_load_file;
-		servers_settings_ = mock_load_file.GetServersConfigs();
+		ParserManager parser("srcs/config_files/AGVTest.conf");
+		servers_settings_ = parser.GetServersSettings();;
 		return true;
 	}
 	return false;
