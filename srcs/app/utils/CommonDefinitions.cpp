35bdadb6a93aebd01b4bdad490dd7a3ed5c3e7f3
Alvaro Ollero <aollero@gmail.com>
2021-10-16 18:34:33 +0300

    no external visibility of Constants:: since we have accessors

--- srcs/app/utils/CommonDefinitions.cpp
+++ srcs/app/utils/CommonDefinitions.cpp
@@ -1,8 +1,19 @@
 #include <CommonDefinitions.hpp>
 
+namespace Constants {
+
+static const char kValidHttpMethods[8][8] = {"GET", "HEAD", "POST",
+	"PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE"};
+
+static const uint16_t kRedirectionReturnStatus[5] = {301,
+	302, 303, 307, 308};
+
+}  // namespace Constants
+
 bool Constants::IsValidMethod(const std::string &method) {
 	const std::size_t	len =
-		sizeof(kValidHttpMethods) / sizeof(kValidHttpMethods[0]);
+		sizeof(Constants::kValidHttpMethods) /
+		sizeof(Constants::kValidHttpMethods[0]);
 	for (std::size_t i = 0; i < len; ++i) {
 		if (method == kValidHttpMethods[i])
 			return true;
--- srcs/incs/CommonDefinitions.hpp
+++ srcs/incs/CommonDefinitions.hpp
@@ -9,12 +9,6 @@ namespace Constants {
 bool IsValidMethod(const std::string &method);
 bool IsReturnStatusRedirection(int16_t status);
 
-const char kValidHttpMethods[8][8] = {"GET", "HEAD", "POST",
-		"PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE"};
-
-const uint16_t kRedirectionReturnStatus[5] = {301,
-		302, 303, 307, 308};
-
 }  // namespace Constants
 
 #endif  // SRCS_INCS_COMMONDEFINITIONS_HPP_
