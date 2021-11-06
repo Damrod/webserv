f8e70236e8c6f665c9176be6d6b36df534f29d14
Victor Dedios <victor.dedios@outlook.com>
2021-11-06 18:58:22 +0100

    #94 create basic FDset container

---
+++ srcs/app/server/FDsets.cpp
@@ -0,0 +1,41 @@
+#include <FDsets.hpp>
+#include <cstring>
+
+FDsets::FDsets() {
+	FD_ZERO(&read_set_);
+	FD_ZERO(&write_set_);
+}
+
+void	FDsets::removeFromReadSet(int fd) {
+	FD_CLR(fd, &read_set_);
+}
+
+void	FDsets::removeFromWriteSet(int fd) {
+	FD_CLR(fd, &write_set_);
+}
+
+void	FDsets::addToWriteSet(int fd) {
+	FD_SET(fd, &write_set_);
+}
+
+void	FDsets::addToReadSet(int fd) {
+	FD_SET(fd, &read_set_);
+}
+
+fd_set	*FDsets::getReadSet() {
+	std::memcpy(&tmp_read_set_, &read_set_, sizeof(read_set_));
+	return &tmp_read_set_;
+}
+
+fd_set	*FDsets::getWriteSet() {
+	std::memcpy(&tmp_write_set_, &write_set_, sizeof(read_set_));
+	return &tmp_write_set_;
+}
+
+bool		FDsets::isReadSet(int fd) const {
+	return FD_ISSET(fd, &tmp_read_set_);
+}
+
+bool		FDsets::isWriteSet(int fd) const {
+	return FD_ISSET(fd, &tmp_write_set_);
+}
