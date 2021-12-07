4676d60e04e1b4faad7628c00ee0f57c499f2a3f
Victor Dedios <victor.dedios@outlook.com>
2021-12-07 14:25:49 +0100

    #130 add integration tests covering principal cases

--- srcs/app/test/integration/server_name_test.py
+++ srcs/app/test/integration/server_name_test.py
@@ -1,38 +1,75 @@
-import os
 from pathlib import Path
 import pytest
 import requests
-import shutil
 import subprocess
 import time
-import uuid
 
-# General integration tests.
+# Before running these tests add the following
+# to /etc/hosts:
+# - 127.0.0.1 example.com
+# - 127.0.0.1 example1.com
+# - 127.0.0.1 example2.com
 
-TMP_WEBSERV_DIR = '/tmp/webserv/'
 PROJ_DIR = str(Path(__file__).parents[4])
-TMP_UPLOAD_DIR = PROJ_DIR + '/html/web3/test/'
 
 @pytest.fixture(scope='module', autouse=True)
 def start_webserv():
-    webserv = subprocess.Popen('./webserv', cwd=PROJ_DIR)
+    webserv = subprocess.Popen(['./webserv', PROJ_DIR + '/config/server_name_test.conf'], cwd=PROJ_DIR)
     time.sleep(2)
     yield webserv
     webserv.terminate()
 
-@pytest.fixture(scope='module')
-def tmp_webserv_dir():
-    if not os.path.exists(TMP_WEBSERV_DIR):
-        os.mkdir(TMP_WEBSERV_DIR)
-    yield
-    shutil.rmtree(TMP_WEBSERV_DIR)
+def test_same_port_different_server_name():
+    response = requests.get('http://example.com:8080/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web4\n'
+    response = requests.get('http://example2.com:8080/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web5\n'
 
-@pytest.fixture(scope='function')
-def random_filename():
-    return str(uuid.uuid4())
+def test_same_port_same_server_name():
+    response = requests.get('http://example.com:8081/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web4\n'
 
-# MISC
-def test_method_not_implemented_501():
-    url = 'http://localhost:8080/'
-    response = requests.options(url)
-    assert response.status_code == 501
+def test_different_port_same_server_name():
+    response = requests.get('http://example.com:8082/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web4\n'
+    response = requests.get('http://example.com:8083/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web5\n'
+
+def test_no_server_name():
+    response = requests.get('http://localhost:8084/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web4\n'
+
+def test_no_server_name_same_port():
+    response = requests.get('http://localhost:8085/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web4\n'
+    response = requests.get('http://example.com:8085/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web4\n'
+
+def test_no_server_name_different_port():
+    response = requests.get('http://localhost:8086/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web4\n'
+    response = requests.get('http://example.com:8087/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web5\n'
+
+def test_wildcard_server_name():
+    response = requests.get('http://example.com:8088/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web4\n'
+    response = requests.get('http://example1.com:8087/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web5\n'
+
+def test_default_server_name():
+    response = requests.get('http://localhost:8088/')
+    assert response.status_code == 200
+    assert response.content.decode('UTF-8') == 'web4\n'
