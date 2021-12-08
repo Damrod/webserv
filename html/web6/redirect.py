1c33eb4a9c95407d39bcabbf87879ad4af71429f
gbudau <54190400+gbudau@users.noreply.github.com>
2021-12-08 13:09:45 +0100

    Update redirect.py
    Use sys.stdout.write instead of print to not append a newline character

--- html/web6/redirect.py
+++ html/web6/redirect.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+import sys
 
-print 'Content-type:text/html\r\n',
-print 'Location: https://www.google.com\r\n',
-print 'Status: 301\r\n\r\n',
+sys.stdout.write('Content-type:text/html\r\n')
+sys.stdout.write('Location: https://www.google.com\r\n')
+sys.stdout.write('Status: 301\r\n\r\n')
